f5ca661e86e14ca33ebfa53d136a0a77cd521d78
Uzeyir <huzarh44@gmail.com>
2024-10-07 19:49:51 +0300

    Görev 1 nextjs txj ile devam ediyorum
    Signed-off-by: Uzeyir <huzarh44@gmail.com>

---
+++ app/pages/about.tsx
@@ -0,0 +1,21 @@
+// pages/about.js
+
+const About = () => {
+    return (
+        <div>
+            <h1>Hakkımda</h1>
+            <p>
+                Merhaba, ben Uzeyir, Front-end {/* asılında nodejs expressjs . restfull api 2 ugulamalı deneyimlerim var*/} alanında deneyimli bir yazılım geliştiricisiyim.
+                Uzmanlıklarım arasında React, Next.js ve kullanıcı arayüzü tasarımı yer almakta. İnsanlara,
+                bu alanda sahip olduğum bilgi ve tecrübelerle yardımcı oluyorum.
+            </p>
+            <p>
+                İhtiyaçlarınıza göre özelleştirilmiş çözümler sunarak
+                size en iyi şekilde destek olabilirim. Detaylar ve hizmetlerim
+                hakkında bilgi almak için lütfen iletişime geçin.
+            </p>
+        </div>
+    );
+};
+
+export default About;
