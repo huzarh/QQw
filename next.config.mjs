7035a36716569b3bf8b1d409a979e192355cc664
Buenyamin Inan <inanbunyamin90@gmail.com>
2024-10-20 17:11:17 +0200

    Fixing docker build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,20 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import path from 'path';
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    };
+
+    return config;
+  },
+};
 
 export default withNextIntl(nextConfig);
