f22f9700c2ce9aac9a9080b3d06b0e529958e324
Uzeyir <huzarh44@gmail.com>
2024-12-28 15:28:15 +0300

    -

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -37,7 +37,8 @@ export default async function RootLayout({
       <NextIntlClientProvider messages={messages}>
         <body
           className={`${geistSans.variable} ${geistMono.variable} p-4 antialiased w-5xl min-w-80`}
-        >
+        >        <meta name="google-site-verification" content="_6hZElubIlAU_jlpUA_eUuZnMudfeAWKbt_f0vwn2MI" />
+
           <Providers>
             <Header />
             {children}
