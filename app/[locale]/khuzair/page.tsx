a78734af708704ea6c89f141fdc9b101c87e2e59
Uzeyir <huzarh44@gmail.com>
2025-01-14 00:44:38 +0300

    added footer

--- app/[locale]/khuzair/page.tsx
+++ app/[locale]/khuzair/page.tsx
@@ -9,9 +9,32 @@ import {
 } from "@/components/ui/card";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import Image from "next/image";
+import Head from "next/head";
 
 export default function EmailNotificationCard() {
   return (
+    <><Head>
+      {/* <!-- Primary Meta Tags --> */}
+    <title>Khuzair Askyer</title>
+    <meta name="title" content="Khuzair Askyer" />
+    <meta name="description" content="Sakarya Uygulamalı Bilimler Üniversitesi'nde Mekatronik  Mühendisliği okuyorum. Teknoloji ve tasarım tutkusuyla, geleceğin elektronik sistemlerini şekillendirmek için çalışıyorum." />
+    
+    {/* <!-- Open Graph / Facebook --> */}
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://uzeyiraskyer.com/en/khuzair" />
+    <meta property="og:title" content="Khuzair Askyer" />
+    <meta property="og:description" content="Sakarya Uygulamalı Bilimler Üniversitesi'nde Mekatronik  Mühendisliği okuyorum. Teknoloji ve tasarım tutkusuyla, geleceğin elektronik sistemlerini şekillendirmek için çalışıyorum." />
+    <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+    
+    {/* <!-- Twitter --> */}
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://uzeyiraskyer.com/en/khuzair" />
+    <meta property="twitter:title" content="Khuzair Askyer" />
+    <meta property="twitter:description" content="Sakarya Uygulamalı Bilimler Üniversitesi'nde Mekatronik  Mühendisliği okuyorum. Teknoloji ve tasarım tutkusuyla, geleceğin elektronik sistemlerini şekillendirmek için çalışıyorum." />
+    <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+    
+    {/* <!-- Meta Tags Generated with https://metatags.io --> */}
+    </Head>
     <div className="min-h-screen bg-transparent flex items-center justify-center p-4">
       <Card className="w-full max-w-md dark:bg-gray-900 dark:border-gray-850">
         <CardHeader className="flex flex-row items-center space-x-4 align-center">
@@ -49,6 +72,6 @@ export default function EmailNotificationCard() {
           </p>
         </CardContent>
       </Card>
-    </div>
+    </div></>
   );
 }
